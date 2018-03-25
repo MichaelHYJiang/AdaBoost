d62a349ceb7092e86371e1f0bc91b4689ffb48f7
Haiyang Jiang <450199156@qq.com>
2018-03-24 17:36:11 -0700

    implement train function and its tester

--- decision_stump.py
+++ decision_stump.py
@@ -5,18 +5,36 @@ class DecisionStump(object):
     def __init__(self):
         self.dim = None
         self.thresh = None
+        self.sign = None
     
     def train(self, trainset, label):
         
         # assert trainset is a list of lists of float numbers
+        # assert labels are +1 and -1
         trainset = np.array(trainset)
         num_samples, num_dim = trainset.shape
         min_error = num_samples
+        best_dim = 0
+        best_thresh = 0
+        best_sign = 1
         for i in range(num_dim):
             possibilities = np.unique(trainset[:, i])
             possibilities.sort()
+            # print('possibilities:', possibilities)
             for j in range(len(possibilities) - 1):
                 threshold = (possibilities[j] + possibilities[j + 1]) / 2
-                error = 0
-                for k in range(num_samples):
-                    error += 0
+                # print('threshold:', threshold)
+                for sign in [+1, -1]:
+                    error = 0
+                    for k in range(num_samples):
+                        # print(int(label[k] != sign * int(int(trainset[k, i] <= threshold) * 2 - 1)))
+                        error += int(label[k] != sign * int((int(trainset[k, i] <= threshold) - 0.5) * 2))
+                    if error < min_error:
+                        min_error = error
+                        best_dim = i
+                        best_thresh = threshold
+                        best_sign = sign
+        # print('min_error:', min_error)
+        self.dim = best_dim
+        self.thresh = best_thresh
+        self.sign = best_sign
--- testers.py
+++ testers.py
@@ -4,7 +4,13 @@ from decision_stump import DecisionStump
 
 class TestDecisionStump(unittest.TestCase):
     def test_train(self):
-        pass
+        ds = DecisionStump()
+        trainset = [[1, 1], [-1, 1], [-1, -1], [1, -1]]
+        labels = [1, -1, -1, 1]
+        ds.train(trainset, labels)
+        self.assertEqual(ds.dim, 0)
+        self.assertEqual(ds.thresh, 0.0)
+        self.assertEqual(ds.sign, -1)
 
 if __name__ == '__main__':
     unittest.main()
